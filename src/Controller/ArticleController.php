3bf9456c15f7dab165469e8162217f9327044f40
alexandre gaillot <gaillotalexandre@yahoo.fr>
2022-05-03 11:03:37 +0200

    update ArticleController et ArticleModel

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,6 +9,12 @@ use Core\Controller\DefaultController;
  */
 final class ArticleController extends DefaultController {
 
+    private ArticleModel $model;
+
+    public function __construct ()
+    {
+        $this->model = new ArticleModel();
+    }
     /**
      * Renvoie l'ensemble des articles 
      *
@@ -16,8 +22,31 @@ final class ArticleController extends DefaultController {
      */
     public function index (): void
     {
-        $this->jsonResponse((new ArticleModel())->findAll());
+        $this->jsonResponse($this->model->findAll());
     }
 
-    
+    public function single (int $id): void
+    {
+        $this->jsonResponse($this->model->find($id));
+    }
+
+    public function save ($article): void
+    {
+        $lastId = $this->model->saveArticle($article);
+        $this->jsonResponse($this->model->find($lastId));
+    }
+
+    public function update (int $id, array $article): void
+    {
+        if ($this->model->updateArticle(id: $id, article: $article)) {
+            $this->jsonResponse($this->model->find($id));
+        }
+    }
+
+    public function delete (int $id): void
+    {
+        if ($this->model->delete($id)) {
+            $this->jsonResponse("Article supprimé avec succés");
+        }
+    }
 }
--- src/Model/ArticleModel.php
+++ src/Model/ArticleModel.php
@@ -11,4 +11,28 @@ final class ArticleModel extends DefaultModel {
 
     protected string $table = "article";
     protected string $entity = "Article";
+
+    public function saveArticle ($article): int|false
+    {
+        $stmt = "INSERT INTO $this->table (title, content, categorie_id) VALUES (:title, :content, :categorie_id)";
+        $prepare = $this->pdo->prepare($stmt);
+        if ($prepare->execute($article)) {
+            return $this->pdo->lastInsertId($this->table);
+        }
+        return false;
+    }
+
+    public function updateArticle(int $id, array $article): bool
+    {
+        $stmt = "
+            UPDATE $this->table SET
+            title = :title,
+            content = :content,
+            categorie_id = :categorie_id
+            WHERE id = :id
+        ";
+        $prepare = $this->pdo->prepare($stmt);
+        $article['id'] = $id;
+        return $prepare->execute($article);
+    }
 }
